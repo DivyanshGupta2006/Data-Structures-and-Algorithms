65e3c6a8dc1e19c5a068f85913865aace8ccf876
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-08-14 12:40:48 +0530

    Add Singly Linked List

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -1,18 +1,352 @@
-// Header file containing declarations of LinkedList class and all the associated functions and variables
+// Header file containing declarations and definitions of LinkedList class and all the associated functions and variables
 
-// include guard to prevent multiple inclusions in different files that are also included in each other
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
+// "include guard" to prevent multiple inclusions in different files that are also included in each other
+#pragma once
+#include <iostream>
 
-// class declaration
+// template class declaration - so the class will be generic with respect to datatypes
+template<typename T>
 class LinkedList {
 private:
     struct Node {
-        int data;
+        // declaration of node variables
+        T value;
         Node *next;
+
+        // constructor of struct - takes reference to a data of type T and give that to value and nullptr to next
+        Node(const T &data) : value(data), next(nullptr) {
+        }
     };
 
+    size_t size;
+
+    // private functions used for merge sort
+    Node *findMiddle(Node* h);
+
+    Node *merge_sort(Node *h);
+
+    Node *merge_sorted(Node *a, Node *b);
+
+public:
     Node *head;
+
+    // constructor
+    LinkedList() : head(nullptr), size(0) {
+    }
+
+    // destructor
+    ~LinkedList();
+
+    // basic functions
+    size_t getSize() const {
+        return size;
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+
+    // utility functions
+    void display() const;
+
+    Node *findMin() const;
+
+    Node *findMax() const;
+
+    // insertion functions
+    void insert_begin(const T &data);
+
+    void insert_end(const T &data);
+
+    void insert_at(const T &data, const size_t index);
+
+    // deletion functions
+    void delete_begin();
+
+    void delete_end();
+
+    void delete_at(const size_t index);
+
+    void delete_element(const T &data);
+
+    // other functions
+    Node *find(const T &data) const;
+
+    void sort();
+
+    void merge(const LinkedList<T> &list);
+
+    void reverse();
 };
 
-#endif
+// defining the destructor
+template<typename T>
+LinkedList<T>::~LinkedList() {
+    Node *current = head;
+    while (current != nullptr) {
+        Node *temp = current;
+        current = current->next;
+        delete temp;
+    }
+    head = nullptr;
+    size = 0;
+}
+
+// defining utility functions
+template<typename T>
+void LinkedList<T>::display() const {
+    Node *current = head;
+    while (current != nullptr) {
+        std::cout << current->value << " ";
+        current = current->next;
+    }
+    std::cout << "\n";
+}
+
+template<typename T>
+typename LinkedList<T>::Node *LinkedList<T>::findMin() const {
+    if (isEmpty()) {
+        throw std::out_of_range("LinkedList is empty.");
+    }
+    Node *current = head;
+    Node *minn = head;
+    while (current != nullptr) {
+        if (current->value < minn->value) {
+            minn = current;
+        }
+        current = current->next;
+    }
+
+    return minn;
+}
+
+template<typename T>
+typename LinkedList<T>::Node *LinkedList<T>::findMax() const {
+    if (isEmpty()) {
+        throw std::out_of_range("LinkedList is empty.");
+    }
+    Node *current = head;
+    Node *maxx = head;
+    while (current != nullptr) {
+        if (current->value > maxx->value) {
+            maxx = current;
+        }
+        current = current->next;
+    }
+
+    return maxx;
+}
+
+// defining insertion functions
+template<typename T>
+void LinkedList<T>::insert_begin(const T &data) {
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    size++;
+}
+
+template<typename T>
+void LinkedList<T>::insert_end(const T &data) {
+    Node *newNode = new Node(data);
+    if (isEmpty()) {
+        head = newNode;
+    } else {
+        Node *current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    size++;
+}
+
+template<typename T>
+void LinkedList<T>::insert_at(const T &data, const size_t index) {
+    if (index == 0) {
+        insert_begin(data);
+        return;
+    } else if (index > size) {
+        throw std::out_of_range("Invalid index: Insertion out of bounds.");
+    } else {
+        Node *newNode = new Node(data);
+        Node *current = head;
+        for (size_t i = 0; i < index - 1; i++) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+        size++;
+    }
+}
+
+// defining deletion functions
+template<typename T>
+void LinkedList<T>::delete_begin() {
+    if (isEmpty()) {
+        throw std::out_of_range("LinkedList is empty.");
+    } else {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::delete_end() {
+    if (isEmpty()) {
+        throw std::out_of_range("LinkedList is empty.");
+    } else {
+        Node *current = head;
+        while (current->next->next != nullptr) {
+            current = current->next;
+        }
+
+        delete current->next;
+        current->next = nullptr;
+        size--;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::delete_at(const size_t index) {
+    if (isEmpty()) {
+        throw std::out_of_range("LinkedList is empty.");
+    } else {
+        Node *current = head;
+        for (size_t i = 0; i < index - 1; i++) {
+            current = current->next;
+        }
+        Node *temp = current->next;
+        current->next = temp->next;
+        delete temp;
+        size--;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::delete_element(const T &data) {
+    Node *current = head;
+    if (current->value == data) {
+        head = head->next;
+        delete current;
+        size--;
+        return;
+    }
+    while (current->next != nullptr) {
+        if (current->next->value == data) {
+            Node *temp = current->next;
+            current->next = temp->next;
+            delete temp;
+            size--;
+            break;
+        }
+        current = current->next;
+    }
+}
+
+// defining other functions
+template<typename T>
+typename LinkedList<T>::Node *LinkedList<T>::find(const T &data) const {
+    Node *current = head;
+    while (current != nullptr) {
+        if (current->value == data) {
+            return current;
+        }
+        current = current->next;
+    }
+
+    return nullptr;
+}
+
+template<typename T>
+typename LinkedList<T>::Node *LinkedList<T>::findMiddle(Node* h) {
+    if (h == nullptr || h->next == nullptr) {
+        return h;
+    }
+
+    // create two pointers to find mid of the list
+    Node *slow_ptr = h;
+    Node *fast_ptr = h->next;
+
+    while (fast_ptr != nullptr && fast_ptr->next != nullptr) {
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+    }
+    return slow_ptr;
+}
+
+template<typename T>
+typename LinkedList<T>::Node *LinkedList<T>::merge_sort(Node *h) {
+    if (h == nullptr || h->next == nullptr) {
+        return h;
+    }
+
+    // find middle
+    Node *middle = findMiddle(h);
+    Node *mid_next = middle->next;
+
+    // break the list
+    middle->next = nullptr;
+
+    Node *left = merge_sort(h);
+    Node *right = merge_sort(mid_next);
+
+    Node *sorted = merge_sorted(left, right);
+    return sorted;
+}
+
+template<typename T>
+typename LinkedList<T>::Node * LinkedList<T>::merge_sorted(Node *a, Node *b) {
+    Node* result = nullptr;
+    if (a == nullptr) {
+        return b;
+    }
+    if (b == nullptr) {
+        return a;
+    }
+    if (a->value <= b->value) {
+        result = a;
+        result->next = merge_sorted(a->next, b);
+    }else {
+        result = b;
+        result->next = merge_sorted(a, b->next);
+    }
+    return result;
+}
+
+template<typename T>
+void LinkedList<T>::sort() {
+    head = merge_sort(head);
+}
+
+template<typename T>
+void LinkedList<T>::merge(const LinkedList<T> &list) {
+    if (list.isEmpty()) {
+        return;
+    } else if (isEmpty()) {
+        head = list.head;
+    } else {
+        Node *current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = list.head;
+        size += list.size;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::reverse() {
+    Node *prev = nullptr;
+    Node *current = head;
+    Node *next = nullptr;
+    while (current != nullptr) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+}
--- main.cpp
+++ main.cpp
@@ -8,9 +8,67 @@ using namespace std;
 
 #define pl(a) cout << a << "\n";
 
-int main() {
-
+void run_test() {
     pl("Run this file to test functionalities")
 
-    return 0;
+    // create a new singly linked list object containing data of int type
+    LinkedList<int> list;
+
+    // insert two elements at beginning
+    list.insert_begin(1);
+    list.insert_begin(2);
+
+    // print list and size
+    list.display();
+    cout << list.getSize() << endl;
+
+    // insert two elements at end
+    list.insert_end(3);
+    list.insert_end(4);
+
+    // print list and size
+    list.display();
+    cout << list.getSize() << endl;
+
+    // insert two elements at index 2
+    list.insert_at(5, 2);
+    list.insert_at(6, 2);
+
+    // print list and size
+    list.display();
+    cout << list.getSize() << endl;
+
+    // delete an element from beginning
+    list.delete_begin();
+
+    // print list and size
+    list.display();
+    cout << list.getSize() << endl;
+
+    // delete an element from end
+    list.delete_end();
+
+    // print list and size
+    list.display();
+    cout << list.getSize() << endl;
+
+    // delete an element at index 1
+    list.delete_at(1);
+
+    // print list and size
+    list.display();
+    cout << list.getSize() << endl;
+
+    // delete elements present and not present in list
+    list.delete_element(5);
+    list.delete_element(0);
+
+    // print list and size
+    list.display();
+    cout << list.getSize() << endl;
+
+    // find elements present and not present in list
+    cout << "Position of '5' is " << list.find(5) << endl;
+    cout << "Position of '6' is " << list.find(6) << endl;
+
 }
